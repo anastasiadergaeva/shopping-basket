2d071369316c417598dafcb5ce454167b0929a01
anastasiadergaeva <adergaeva@gmail.com>
2023-10-21 13:10:53 +0300

    fix function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
 const price = document.querySelectorAll('.buycard__price_value');
-const firstPrice = Number((Price[0].innerText).slice(0, -1));
-const secondPrice = Number((Price[1].innerText).slice(0, -1));
-const thirdPrice = Number((Price[2].innerText).slice(0, -1));
-const fourthPrice = Number((Price[3].innerText).slice(0, -1));
+const firstPrice = Number((price[0].innerText).slice(0, -4));
+const secondPrice = Number((price[1].innerText).slice(0, -4));
+const thirdPrice = Number((price[2].innerText).slice(0, -4));
+const fourthPrice = Number((price[3].innerText).slice(0, -4));
 const result = firstPrice + secondPrice + thirdPrice + fourthPrice;
 
 const resultTotal = document.querySelector('#result__total');
-resultTotal.textContent = String(result) + 'р';
+resultTotal.textContent = String(result) + ' RUB';
 
 function getDiscount() {
-    resultTotal.textContent = String(result * 0.8) + 'руб.';
+    resultTotal.textContent = String(result * 0.8) + ' RUB';
     const newPrice = document.querySelector('.coupon');
     newPrice.textContent = 'Вы применили скидку :)';
 };
